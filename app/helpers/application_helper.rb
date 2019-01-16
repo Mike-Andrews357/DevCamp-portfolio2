d193a87bd9766732d8f02c4449af9f7552ea5fec
Michael Andrews <mike.andrews357@gmail.com>
2019-01-15 21:22:57 -0500

    Refactored alerts into helpr method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -53,10 +53,22 @@ def nav_items
       title: 'Portfolio'
     }
   ]
-end
+  end
 
   def active? path 
   "active" if current_page? path
+  end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Mike Andrews Portfolio", sticky: false)
   end
 
 end
